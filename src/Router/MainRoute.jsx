9492a97ead228b01d481c283130ca80bc038af6c
FiaruProject <111216829+FiaruProject@users.noreply.github.com>
2025-11-12 18:22:26 +0600

    react-router set up done

---
+++ src/Router/MainRoute.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../LayOut.jsx/Root";
+
+export let router = createBrowserRouter([
+    {
+        path:"/",
+        Component:Root
+    }
+])
